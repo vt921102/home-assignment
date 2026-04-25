1fcf5a7eb5a93207b4bef2edca79fcbaf607bc0b
Toan Le <toan.le@ataccama.com>
2026-04-25 10:26:22 +0700

    HOME: init project on master

---
+++ src/test/java/com/toanlv/homeassignment/HomeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toanlv.homeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
